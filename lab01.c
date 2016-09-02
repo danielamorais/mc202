a079b7cd89a9a862457985a1669676f470d2b3e8
Daniela Morais <dani.m@hotmail.co.uk>
2016-09-01 23:09:20 -0300

    Refatorar para struct

--- lab01.c
+++ lab01.c
@@ -1,42 +1,85 @@
 #include <stdio.h>
 
 FILE *arquivoA, *arquivoB;
-int conteudo;
-int linhaA, linhaB, colunaA, colunaB;  
+//int conteudo;
+//int linhaA, linhaB, colunaA, colunaB;  
+
+typedef struct matriz {
+	int linha; 
+	int coluna;
+	int **dados; 
+	FILE *arquivo; 
+} Matriz; 
+
+FILE * abrirArquivo(char *nome){
+	FILE *arquivo;
+	arquivo = fopen(nome, "r");
+	if(arquivo == NULL){
+		perror("Erro ao abrir arquivo:");
+		return NULL; //fixme 
+	}else{
+		return arquivo; 
+	}
+}
+
+Matriz lerMatriz(FILE *arquivo){
+	int conteudo = 0;
+	int contadorLinhas = 0; 
+	Matriz matriz;  
+	while(fscanf(arquivo, "%d", &conteudo) != EOF){
+		++contadorLinhas;
+		//printf("%d \n", conteudo);
+		switch(contadorLinhas){
+			case 1: matriz.linha = conteudo;
+				break;
+			case 2: matriz.coluna = conteudo;
+				break; 
+		}
+	}
+	printf("Linha %d: ", matriz.linha);
+	printf("Coluna %d: ", matriz.coluna);
+	return matriz; 
+}
+
 void main(){
-		arquivoA = fopen("matrizA.txt", "r");
-		if(arquivoA == NULL){
-				perror("matrizA.txt:");
-		}
-		arquivoB = fopen("matrizB.txt", "r");
-		if(arquivoB == NULL){
-				perror("matrizB.txt:");
-		}else{
-				int contPrimeiraLinha = 0; 
-				int contPrimeiraColuna = 0; 
-				while(fscanf(arquivoA, "%d", &conteudo) != EOF){
-						printf("%d", conteudo);
-						
-						++contPrimeiraLinha;
-						++contPrimeiraColuna; 
-						/* printf("Coluna: %d Character: %d", contPrimeiraColuna, conteudo); 
-						if(contPrimeiraColuna == 3){
-								printf("Entao a linha eh %d", contPrimeiraLinha); 
-						}
-						*/
-						if(contPrimeiraLinha == 1){
-								if (contPrimeiraColuna == 1){
-										linhaA = (int) conteudo;
-								} else if(contPrimeiraColuna == 3){
-										printf("entrou aqui"); 
-										colunaA = (int) conteudo;
-								}
-						}
-						 
-				}
-				printf("Linha A %d \n", linhaA);
-				printf("Coluna A %d \n", colunaA); 
-				fclose(arquivoA);
-				fclose(arquivoB);		 
-		}
+	Matriz matrizA;
+	Matriz matrizB;
+	matrizA.arquivo = abrirArquivo("matrizA.txt");
+	matrizB.arquivo = abrirArquivo("matrizB.txt");
+	if(matrizA.arquivo != NULL && matrizB.arquivo != NULL){
+		lerMatriz(matrizA.arquivo);
+		lerMatriz(matrizB.arquivo);
+		/		/*  */  
+			//		int contPrimeiraLinha = 0; 
+			//		int contPrimeiraColuna = 0; 
+			//		while(fscanf(arquivoA, "%d", &conteudo) != EOF){
+			//			printf("%d", conteudo);
+			//
+			//			++contPrimeiraLinha;
+			//			++contPrimeiraColuna; 
+			//			   printf("Coluna: %d Character: %d", contPrimeiraColuna, conteudo); 
+			//			   if(contPrimeiraColuna == 3){
+			//			   printf("Entao a linha eh %d", contPrimeiraLinha); 
+			//			   }
+			//			   
+			//			if(contPrimeiraLinha == 1){
+			//				if (contPrimeiraColuna == 1){
+			//					linhaA = (int) conteudo;
+			//				} else if(contPrimeiraColuna == 3){
+			//					printf("entrou aqui"); 
+			//					colunaA = (int) conteudo;
+			//				}
+			//			}
+			//
+			//		}
+			printf("Linha A %d \n", linhaA);
+		printf("Coluna A %d \n", colunaA);
+
+		printf("Deu certo!\n");
+		*/
+			fclose(matrizA.arquivo);
+		fclose(matrizB.arquivo);		 
+	}else{
+		printf("Erro ao acessar arquivo!");
+	}
 }
