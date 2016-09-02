162a67441f6f84b2fdaba2ae2dec2dc3958f9591
Daniela Morais <dani.m@hotmail.co.uk>
2016-09-02 00:59:17 -0300

    Implementacao de matriz dinamica

--- lab01.c
+++ lab01.c
@@ -1,8 +1,11 @@
+/* 
+Daniela Marques de Morais - 169562
+Programa para calcular a multiplicação entre matrizes 
+*/
+
 #include <stdio.h>
 
 FILE *arquivoA, *arquivoB;
-//int conteudo;
-//int linhaA, linhaB, colunaA, colunaB;  
 
 typedef struct matriz {
 	int linha; 
@@ -22,18 +25,24 @@ FILE * abrirArquivo(char *nome){
 	}
 }
 
-Matriz lerMatriz(FILE *arquivo){
+Matriz lerMatriz(Matriz *matriz){
 	int conteudo = 0;
-	int contadorLinhas = 0; 
-	Matriz matriz;  
-	while(fscanf(arquivo, "%d", &conteudo) != EOF){
+	int contadorLinhas = 0;   
+	while(fscanf(matriz->arquivo, "%d", &conteudo) != EOF){
 		++contadorLinhas;
-		//printf("%d \n", conteudo);
 		switch(contadorLinhas){
-			case 1: matriz.linha = conteudo;
+			case 1: matriz->linha = conteudo;
 				break;
-			case 2: matriz.coluna = conteudo;
-				break; 
+			case 2: matriz->coluna = conteudo;
+				int *linha = (int *) malloc(matriz->linha * sizeof(int));
+				for(int i =0; i < matriz->coluna; i++){
+					linha[i] = (int *) malloc(matriz->coluna * sizeof(int);		
+				}
+				matriz->dados = linha; 
+				break;
+			default:
+				
+				break;  
 		}
 	}
 	printf("Linha %d: ", matriz.linha);
@@ -49,35 +58,8 @@ void main(){
 	if(matrizA.arquivo != NULL && matrizB.arquivo != NULL){
 		lerMatriz(matrizA.arquivo);
 		lerMatriz(matrizB.arquivo);
-		/		/*  */  
-			//		int contPrimeiraLinha = 0; 
-			//		int contPrimeiraColuna = 0; 
-			//		while(fscanf(arquivoA, "%d", &conteudo) != EOF){
-			//			printf("%d", conteudo);
-			//
-			//			++contPrimeiraLinha;
-			//			++contPrimeiraColuna; 
-			//			   printf("Coluna: %d Character: %d", contPrimeiraColuna, conteudo); 
-			//			   if(contPrimeiraColuna == 3){
-			//			   printf("Entao a linha eh %d", contPrimeiraLinha); 
-			//			   }
-			//			   
-			//			if(contPrimeiraLinha == 1){
-			//				if (contPrimeiraColuna == 1){
-			//					linhaA = (int) conteudo;
-			//				} else if(contPrimeiraColuna == 3){
-			//					printf("entrou aqui"); 
-			//					colunaA = (int) conteudo;
-			//				}
-			//			}
-			//
-			//		}
-			printf("Linha A %d \n", linhaA);
-		printf("Coluna A %d \n", colunaA);
-
 		printf("Deu certo!\n");
-		*/
-			fclose(matrizA.arquivo);
+		fclose(matrizA.arquivo);
 		fclose(matrizB.arquivo);		 
 	}else{
 		printf("Erro ao acessar arquivo!");
