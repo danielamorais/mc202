7e7679ae02aa55a0a635e971f21ff37b22d82b77
Daniela Morais <dani.m@hotmail.co.uk>
2016-09-16 01:59:23 -0300

    Adicionando lista na estrutura de dados

--- 02/lab03.c
+++ 02/lab03.c
@@ -16,7 +16,7 @@ Lista *lista;
 Requisicao *listaRequisicao; 
 
 void inicializarLista(Lista **lista){
-	*lista = NULL;
+	*lista = NULL;	
 }
 
 void inicializarListaRequisicao(Requisicao **lista){
@@ -24,10 +24,37 @@ void inicializarListaRequisicao(Requisicao **lista){
 }
 
 void adicionarElemento(int numero, Lista **lista){
+	if((*(*lista)).prox == NULL){
+		Lista *novo = malloc(sizeof(Lista));
+		if(novo == NULL){
+			printf("Erro ao alocar memoria!");
+			exit(1);
+		}
+		novo->dado = numero;
+		novo->prox = NULL;
+		(*lista)->prox = novo; 
+	}else{
+		Lista *aux = (*lista)->prox;
+		adicionarElemento(numero, &aux);
+	}	
+}
 
+void adicionarPrimeiroElemento(int numero, Lista **lista){
+	Lista *novo = malloc(sizeof(Lista));
+	if(novo == NULL){
+		printf("Erro ao alocar memoria!");
+		exit(1);
+	}
+	novo->dado = numero;
+	novo->prox = NULL;
+	*lista = novo;  
 }
 
 void adicionarRequisicao(int requisicao, int numero, Requisicao **listaRequisicao){
+
+}
+
+void adicionarPrimeiraRequisicao(int requisicao, int numero, Requisicao **listaRequisicao){
 
 }
 
@@ -42,16 +69,25 @@ void leituraItens(){
 	scanf(" %d %d", &n, &r);
 	for(int i = 0; i < n; i++){
 		scanf(" %d", &numero);
-		adicionarElemento(numero, &lista);
+		printf("Lendo .. %d \n", numero);
+		i == 0 ? adicionarPrimeiroElemento(numero, &lista) : adicionarElemento(numero, &lista);
 	}
 	for(int i = 0; i < r; i++){
 		scanf(" %c %d", &requisicao, &numero);
-		adicionarRequisicao(requisicao, numero, &listaRequisicao);
+		printf("Lendo2 .. %d \n", numero);
+		i == 0 ? adicionarPrimeiraRequisicao(requisicao, numero, &listaRequisicao) : adicionarRequisicao(requisicao, numero, &listaRequisicao);
 	} 
 
 }
 
 int main(){
 	leituraItens();
+	//Exibir itens 
+	Lista *atual = lista;
+	printf("Printando \n");
+	while(atual != NULL){
+		printf("%d ", atual->dado);
+		atual = atual->prox;
+	}
 	return 0;
 }
