a1f3981c537d7f4b6b1a762e413c30e51bf0a8fc
Daniela Morais <dani.m@hotmail.co.uk>
2016-09-16 01:12:47 -0300

    Leitura de itens

--- 02/lab03.c
+++ 02/lab03.c
@@ -1,7 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+typedef struct Lista{
+	int dado;
+	struct Lista *prox;
+} Lista;
 
+typedef struct Requisicao{
+	char tipo;
+	int elemento;
+	struct Requisicao *prox; 
+} Requisicao;
+
+Lista *lista; 
+Requisicao *listaRequisicao; 
+
+void inicializarLista(Lista **lista){
+	*lista = NULL;
+}
+
+void inicializarListaRequisicao(Requisicao **lista){
+	*lista = NULL;
+}
+
+void adicionarElemento(int numero, Lista **lista){
+
+}
+
+void adicionarRequisicao(int requisicao, int numero, Requisicao **listaRequisicao){
+
+}
+
+void leituraItens(){
+	inicializarLista(&lista);
+	inicializarListaRequisicao(&listaRequisicao);
+	int n = 0; //tamanho
+	int r = 0; //requisicoes 
+	char requisicao;
+	int numero;
+
+	scanf(" %d %d", &n, &r);
+	for(int i = 0; i < n; i++){
+		scanf(" %d", &numero);
+		adicionarElemento(numero, &lista);
+	}
+	for(int i = 0; i < r; i++){
+		scanf(" %c %d", &requisicao, &numero);
+		adicionarRequisicao(requisicao, numero, &listaRequisicao);
+	} 
+
+}
+
+int main(){
+	leituraItens();
 	return 0;
 }
