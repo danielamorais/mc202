22f86635bbcb66f2015879315d8228436c947663
Daniela Morais <dani.m@hotmail.co.uk>
2016-09-20 20:50:12 -0300

    Validando matrioshka

--- 03/lab04.c
+++ 03/lab04.c
@@ -32,11 +32,50 @@ void imprimirPilha(Brinquedo **pilha){
 void lerEntrada(Brinquedo **pilha){
 	int numero; 
 	char proxChar;
-
+	int tamanho; 
+	scanf("%d", &tamanho);
+	
 	while(scanf("%d%c", &numero, &proxChar) == 2){
 		adicionarElemento(pilha, numero);
 		if(proxChar == '\n' || proxChar == '\r')
 			break;
+	}
+}
+
+void removerElemento(Brinquedo **pilha){
+	Brinquedo *aux = *pilha;
+	*pilha = aux->prox;
+	free(aux);	
+}
+
+//Verifica se a sequencia eh invalida, isto eh: Foi aberto e fechado o brinquedo
+void validarMatrioshka(Brinquedo **pilha){
+	Brinquedo *aux = *pilha; 
+	Brinquedo *pilhaTemporaria;
+	printf("Inicializando lista...\n");
+	inicializarLista(&pilhaTemporaria);
+	while(aux != NULL){
+		printf("Entrei no while!\n");
+		if(aux->valor >= 0){
+			printf("Adicionado elemento positivo.. %d \n", aux->valor);
+			adicionarElemento(&pilhaTemporaria, aux->valor);
+		}else{
+			if((*pilhaTemporaria).valor == ((aux->valor) * (-1))){
+			 	//Removera item da lista temporaria
+			 	printf("Removendo.. %d \n", (aux->valor) * (-1));
+				removerElemento(&pilhaTemporaria);
+			}else{
+				printf("Erro! %d nao eh igual a %d", (*pilhaTemporaria).valor, aux->valor);
+			}
+		}
+		aux = aux->prox;
+	}	
+	printf("Validando listaTempo....\n");
+	//Se lista temporaria for vazia, entao a lista eh valida
+	if(pilhaTemporaria == NULL){
+		printf("\nValido!");
+	}else{
+		printf("\nINVALIDO");
 	}
 }
 
@@ -45,5 +84,6 @@ int main(){
 	inicializarLista(&pilha);
 	lerEntrada(&pilha);
 	imprimirPilha(&pilha);
+	validarMatrioshka(&pilha);
 	return 0;
 }
