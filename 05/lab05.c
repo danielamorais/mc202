49f144ccca7269aea32083a7a17341febf32a867
Daniela Morais <dani.m@hotmail.co.uk>
2016-10-12 02:45:06 -0300

    Tentando fazer leitura de itens

--- 05/lab05.c
+++ 05/lab05.c
@@ -1,22 +1,46 @@
+/**
+ * Daniela Marques de Morais - 169562
+ * */
+
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef struct ArvoreArquivo{
-	char[20] nome;
-	int quantidade;
-	struct ArvoreArquivo *esq, *dir;
-	enum { MENOS, ZERO, MAIS } fator;
-} *ArvoreArquivo; 
+    char nome[20];
+    int quantidade;
+    struct ArvoreArquivo *esq, *dir;
+    enum { MENOS, ZERO, MAIS } fator;
+} *ArvoreArquivo;
 
-void iniciarArvore(ArvoreArquivo **raiz){ 
-	*raiz = NULL; 
-} 
+void iniciarArvore(ArvoreArquivo **raiz){
+    *raiz = NULL;
+}
 
 //TODO
 void destruirArvore(ArvoreArquivo **raiz){
 
 }
 
-int main(){ 
-	return 0; 
+/**
+ * Faz a leitura dos comandos com os respectivos nomes dos arquivos
+ * */ 
+void leituraItens(){
+    char comando[2];
+    int numero;
+    char arquivo[20];
+    char fimDaLinha = ' ';
+
+    while(scanf("%s %s", comando, arquivo) == 2){
+       printf("Comando %s \t Arquivo %s\n", comando, arquivo);
+       /* if(fimDaLinha == '\n' || fimDaLinha == '\r'){ */
+       /*      printf("Thats all folks"); */
+       /*      break; */
+       /* } */
+    
+    }
+}
+
+int main(){
+    leituraItens();
+    return 0;
 }
